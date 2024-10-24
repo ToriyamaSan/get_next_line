bf989658d3cef2469c745da2c8082f94cdbb6b38
David <lefur.david@bluewin.ch>
2024-10-24 21:48:02 +0200

    1

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dle-fur <dle-fur@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 18:58:28 by dle-fur           #+#    #+#             */
+/*   Updated: 2024/10/24 19:00:48 by dle-fur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h> //permet d ouvrir un fichier
+
+#endif
