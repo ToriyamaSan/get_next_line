56198cc5959f346a09842fa1d6776c1ba3651523
David Le Fur <dle-fur@c2r4s2.42lausanne.ch>
2024-10-25 14:18:26 +0200

    2

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dle-fur <dle-fur@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 18:58:28 by dle-fur           #+#    #+#             */
-/*   Updated: 2024/10/25 11:52:43 by dle-fur          ###   ########.fr       */
+/*   Created: 2024/10/25 14:16:46 by dle-fur           #+#    #+#             */
+/*   Updated: 2024/10/25 14:17:01 by dle-fur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h> //permet d ouvrir un fichier
-
-char	*get_next_line(int fd);
-//utils//
-int		ft_strlen(const char *str);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
-
-#endif
+#include "get_next_line.h"
---
+++ get_next_line_utils.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dle-fur <dle-fur@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 10:59:04 by dle-fur           #+#    #+#             */
+/*   Updated: 2024/10/25 13:17:22 by dle-fur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc(sizeof(char) * ft_strlen(s + 1));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	ch;
+
+	i = 0;
+	ch = c;
+	if (ch == '\0')
+	{
+		i = ft_strlen(s);
+		return ((char *)s + i++);
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ch)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int		i;
+	int		j;
+	char	*dest;
+
+	i = 0;
+	j = 0;
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		dest[i + j] = s2[j];
+		i++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
